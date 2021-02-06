9d53b8cd2eb78c51fa44c40a5a980969f11a6823
AZX309 <74437643+AZX309@users.noreply.github.com>
2021-02-06 13:11:02 -0500

    fixed logout functionality

--- src/webapp/views.py
+++ src/webapp/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.shortcuts import redirect
 
 from src.WebAppraisal.forms import NewUserForm, UpdateAccountForm, UpdatePhoneNumberForm
 from src.webapp.models import Profile
